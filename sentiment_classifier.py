0748ef25d4642d3eca85f3c91f74c8e84d045080
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-08-03 12:50:05 +0700

    Auto train model if it is not trained. Closes #2

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,9 +1,20 @@
 import joblib
+from parsing_reviews import get_reviews
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from pathlib import Path
 
 
 class SentimentClassifier(object):
     def __init__(self):
-        self.pipeline = joblib.load('pipeline.pkl')
+        filename = 'pipeline.pkl'
+
+        trained_model = Path(filename)
+        if trained_model.is_file():
+            self.pipeline = joblib.load(filename)
+        else:
+            self.train_classifier()
 
     @staticmethod
     def get_prediction_text(prediction):
@@ -25,3 +36,14 @@ class SentimentClassifier(object):
     def get_prediction_message(self, text):
         prediction = self.predict_text(text)
         return self.get_prediction_text(prediction)
+
+    def train_classifier(self):
+        train = get_reviews()
+
+        print("Start training classifier")
+        self.pipeline = Pipeline([('vectorizer', TfidfVectorizer(analyzer='char', max_df=0.8, ngram_range=(1, 15))),
+                                  ('classifier', SGDClassifier(random_state=42))])
+        self.pipeline.fit(train.text, train.label)
+
+        joblib.dump(self.pipeline, 'pipeline.pkl')
+        print("Classifier training completed")
